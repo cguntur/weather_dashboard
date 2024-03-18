5ccdaafc6aa15afc6ae26a7b7386b36623780a14
Chandrika Sista <cguntur@gmail.com>
2024-03-17 21:46:18 -0700

    Added code to generate the five day forecast and create cards for each day

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 var today = new Date();
 console.log("Today: " + today.toLocaleDateString());
 //dateStr.toLocaleDateString()
+var five_days_data = [];
+var weatherHistory = JSON.parse(localStorage.getItem('weatherHistory')) || [];
+//var weatherHistory = [];
+var city = "";
 
 generate_countries_dropdown();
 
 document.getElementById("form_submit").addEventListener("click", function(event){
   event.preventDefault();
   console.log("Button Clicked!");
-  var city = document.getElementById("city_input").value;
+  city = document.getElementById("city_input").value;
   var country = document.getElementById("select_country").value;
-  console.log("Country: " + country);
-
   fetch('https://api.openweathermap.org/geo/1.0/direct?q='+city+','+country+'&appid=50935aee1783372b16bb0fdccd4bd624',{
   }).then(function (response){
       return response.json();
@@ -29,7 +31,6 @@ function get_current_weather_forecast(lat, lon){
       return response.json();
   })
   .then(function (data) {
-      //console.log(data);
       display_current_weather_data(data);
   });
 }
@@ -42,6 +43,7 @@ function get_five_day_weather_forecast(lat, lon){
   })
   .then(function (data) {
       console.log(data);
+      //display_five_days_weather(data);
       display_five_days_weather(data);
   });
 }
@@ -61,9 +63,96 @@ function display_current_weather_data(data){
   });
 }
 
-function display_five_days_weather(data){
+function get_five_days_weather_details(data){
   Object.entries(data).forEach(([key, value]) => {
+      var list = data.list;
+      //console.log(list);
+      
+      Object.entries(list).forEach(([key, value]) => {
+          var date = value.dt_txt;
+          date = date.toString("YYYY-MM-DD");
+          date = new Date(date).toLocaleDateString();
+
+          for (i=0; i < list.length; i++) {
+              if ((!list[i].hasOwnProperty(date))) {
+                  five_days_data[date] = value;
+              } 
+          }
+      })
   });
+  console.log("New Data");
+  console.log(five_days_data);
+  return five_days_data;
+}
+
+function display_five_days_weather(data){
+  var five_day_data = get_five_days_weather_details(data);
+  Object.entries(five_day_data).forEach(([key, value]) => {
+      var date = key;
+      console.log("Date: " + key);
+      var max_temp = value.main.temp_max;
+      var min_temp = value.main.temp_min;
+      var humidity = value.main.humidity;
+      var wind_speed = value.wind.speed;
+
+      create_weather_card(date, max_temp, min_temp, humidity, wind_speed);
+
+      console.log("Max Temp: " + max_temp);
+  });
+  
+}
+
+function saveWeatherData(data, five_day_data){
+}
+
+function create_weather_card(date, max_temp, min_temp, humidity, wind_speed){
+  var cardsEl = document.getElementById("five_day_weather");
+
+  var cardDiv = document.createElement("div");
+  cardDiv.classList.add("card");
+
+  var cardSection = document.createElement("div");
+  cardDiv.append(cardSection);
+  cardSection.classList.add("card-section");
+  
+  var cardTitle = document.createElement("h4");
+  cardSection.append(cardTitle);
+  cardTitle.textContent = date;
+
+  var tempIcon = document.createElement("img");
+  tempIcon.classList.add("icon");
+
+  var tempEl = document.createElement("p");
+  tempEl.classList.add("temp");
+  tempEl.textContent = max_temp;
+
+  var windEl = document.createElement("p");
+  windEl.classList.add("wind");
+  windEl.textContent = wind_speed + "mph";
+
+  var humidityEl = document.createElement("p");
+  humidityEl.classList.add("humidity");
+  humidityEl.textContent = humidity + "%";
+
+  cardSection.append(tempIcon);
+  cardSection.append(tempEl);
+  cardSection.append(windEl);
+  cardSection.append(humidityEl);
+
+
+  
+  cardsEl.append(cardDiv);
+
+  //<div class="card" style="width: 300px;">
+  //<div class="card-divider">
+  //    This is a header
+  //</div>
+  //<img src="assets/img/generic/rectangle-1.jpg">
+  //<div class="card-section">
+  //    <h4>This is a card.</h4>
+  //    <p>It has an easy to override visual style, and is appropriately subdued.</p>
+  //</div>
+  //</div>
 }
 
 function generate_countries_dropdown(){
@@ -88,3 +177,15 @@ function generate_countries_dropdown(){
       });
   });
 }
+
+
+function removeusingSet(arr) {
+  let unique = [];
+  arr.forEach(element => {
+      if (!unique.includes(element)) {
+          unique.push(element);
+      }
+  });
+  return unique;
+}
+
