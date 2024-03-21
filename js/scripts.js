c7ddec45219abb4ee719af4f5bd3229a07ae8963
Chandrika Sista <cguntur@gmail.com>
2024-03-21 14:30:24 -0700

    Fixed the issues with the display of five day weather cards

--- js/scripts.js
+++ js/scripts.js
@@ -3,38 +3,34 @@ var five_days_data = [];
 var weatherHistory = JSON.parse(localStorage.getItem('weatherHistory')) || [];
 var city = "";
 
+var cardsEl = document.getElementById("five_day_weather");
+
 generate_countries_dropdown();
 display_past_searched_places();
 display_weather_for_past_places();
 
 document.getElementById("form_submit").addEventListener("click", function(event){
   event.preventDefault();
-  console.log("Button Clicked!");
   city = document.getElementById("city_input").value;
   var country = document.getElementById("select_country").value;
   fetch('https://api.openweathermap.org/geo/1.0/direct?q='+city+','+country+'&appid=50935aee1783372b16bb0fdccd4bd624',{
   }).then(function (response){
       return response.json();
   }).then(function(data){
-    console.log(data);
       var lat = data[0].lat;
       var lon = data[0].lon;
-      console.log("City: " + city);
       get_current_weather_forecast(lat, lon);
       get_five_day_weather_forecast(lat, lon);
-      create_past_search_list(city, country, lat,lon);
       
       var obj = weatherHistory.find(function(entry) { 
-        console.log(entry);
         return entry.city === city; 
       });
       if (obj) {
-        console.log("City already exists.");
         //obj.value = task;
       } else {
         weatherHistory.push({city: city, country: country, lat: lat, lon: lon});
+        create_past_search_list(city, country, lat,lon);
       }
-
       localStorage.setItem("weatherHistory", JSON.stringify(weatherHistory));
   });
 })
@@ -51,13 +47,14 @@ function get_current_weather_forecast(lat, lon){
 }
 
 function get_five_day_weather_forecast(lat, lon){
+    console.log("Five day weather");
   fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&units=imperial&cnt=40&appid=50935aee1783372b16bb0fdccd4bd624',{
   })
   .then(function(response){
       return response.json();
   })
   .then(function (data) {
-      display_five_days_weather(data);
+    display_five_days_weather(data);
   });
 }
 
@@ -77,17 +74,19 @@ function display_current_weather_data(data){
 }
 
 function get_five_days_weather_details(data){
+    console.log("get_five_days_weather_details");
   Object.entries(data).forEach(([key, value]) => {
       var list = data.list;
-      //console.log(list);
-      
+    //  console.log(list);
       Object.entries(list).forEach(([key, value]) => {
           var date = value.dt_txt;
           date = date.toString("YYYY-MM-DD");
           date = new Date(date).toLocaleDateString();
+          //console.log(date);
 
           for (i=0; i < list.length; i++) {
               if ((!list[i].hasOwnProperty(date))) {
+                //console.log("inside if condition");
                   five_days_data[date] = value;
               } 
           }
@@ -98,53 +97,80 @@ function get_five_days_weather_details(data){
 
 function display_five_days_weather(data){
   var five_day_data = get_five_days_weather_details(data);
+  cardsEl.replaceChildren();
+  //console.log(five_day_data);
   Object.entries(five_day_data).forEach(([key, value]) => {
+    //console.log(key);
+    //console.log(value);
       var date = key;
       var max_temp = value.main.temp_max;
       var min_temp = value.main.temp_min;
       var humidity = value.main.humidity;
       var wind_speed = value.wind.speed;
-
-      create_weather_card(date, max_temp, min_temp, humidity, wind_speed);
+      var weatherCond = value.weather[0].main;
+      console.log("Weather Conditions: " + weatherCond);
+      create_weather_card(date, max_temp, min_temp, humidity, wind_speed, weatherCond);
   });
   
 }
 
-function create_weather_card(date, max_temp, min_temp, humidity, wind_speed){
-  var cardsEl = document.getElementById("five_day_weather");
+function create_weather_card(date, max_temp, min_temp, humidity, wind_speed, weatherCond){
+    var icon = "";
+    var cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
 
-  var cardDiv = document.createElement("div");
-  cardDiv.classList.add("card");
+    var cardSection = document.createElement("div");
+    cardDiv.append(cardSection);
+    cardSection.classList.add("card-section");
+    
+    var cardTitle = document.createElement("h4");
+    cardSection.append(cardTitle);
+    cardTitle.textContent = date;
 
-  var cardSection = document.createElement("div");
-  cardDiv.append(cardSection);
-  cardSection.classList.add("card-section");
-  
-  var cardTitle = document.createElement("h4");
-  cardSection.append(cardTitle);
-  cardTitle.textContent = date;
+    var tempIcon = document.createElement("p");
+    tempIcon.classList.add("icon");
 
-  var tempIcon = document.createElement("img");
-  tempIcon.classList.add("icon");
+    switch (weatherCond){
+      case "Clouds":
+        icon = '<i class="fa-solid fa-cloud"></i>';
+        break;
 
-  var tempEl = document.createElement("p");
-  tempEl.classList.add("temp");
-  tempEl.textContent = max_temp;
+      case "Rain":
+        icon = '<i class="fa-solid fa-cloud-showers-heavy"></i>';
+        break;
 
-  var windEl = document.createElement("p");
-  windEl.classList.add("wind");
-  windEl.textContent = wind_speed + "mph";
+      case "Clear": 
+        icon = '<i class="fa-regular fa-sun"></i>';
+        break;
 
-  var humidityEl = document.createElement("p");
-  humidityEl.classList.add("humidity");
-  humidityEl.textContent = humidity + "%";
+      case "Snow": 
+        icon = '<i class="fa-solid fa-snowflake"></i>';
+        break;
 
-  cardSection.append(tempIcon);
-  cardSection.append(tempEl);
-  cardSection.append(windEl);
-  cardSection.append(humidityEl);
+   default: 
+      icon = '<i class="fa-regular fa-sun"></i>';
+}
 
-  cardsEl.append(cardDiv);
+    tempIcon.innerHTML = icon;
+
+    var tempEl = document.createElement("p");
+    tempEl.classList.add("temp");
+    tempEl.textContent = "Max Temp: " + max_temp;
+    
+    var windEl = document.createElement("p");
+    windEl.classList.add("wind");
+    windEl.textContent = "Wind Speed: " + wind_speed + "mph";
+
+    var humidityEl = document.createElement("p");
+    humidityEl.classList.add("humidity");
+    humidityEl.textContent = "Humidity: " + humidity + "%";
+
+    cardSection.append(tempIcon);
+    cardSection.append(tempEl);
+    cardSection.append(windEl);
+    cardSection.append(humidityEl);
+
+    cardsEl.append(cardDiv);
 }
 
 function generate_countries_dropdown(){
